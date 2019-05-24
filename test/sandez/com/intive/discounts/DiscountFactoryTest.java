f2c2593691c67b4d6d3e9525a0dab9cf580f12b0
Jorge Sandez <sandez.jorge@gmail.com>
2019-05-24 18:40:14 -0300

    test for discount factory

---
+++ test/sandez/com/intive/discounts/DiscountFactoryTest.java
@@ -0,0 +1,14 @@
+package sandez.com.intive.discounts;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiscountFactoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
